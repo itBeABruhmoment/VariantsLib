0f823813e227498c8e60656f8eb85f1910fc17ef
itBeABruhmoment <reee271314@gmail.com>
2024-10-27 19:59:32 -0700

    fixed sMods in modded_variants not loading correctly and added better error messages

--- jars/VariantsLibProject/src/main/java/variants_lib/data/ModdedVariantsData.java
+++ jars/VariantsLibProject/src/main/java/variants_lib/data/ModdedVariantsData.java
@@ -1,5 +1,6 @@
 package variants_lib.data;
 
+import java.io.IOException;
 import java.util.HashMap;
 
 import org.apache.log4j.Level;
@@ -39,7 +40,7 @@ public class ModdedVariantsData {
             return false;
         }
 
-        String path = CommonStrings.MODDED_VARIANTS_FOLDER_PATH + variantId + ".variant";
+        final String path = CommonStrings.MODDED_VARIANTS_FOLDER_PATH + variantId + ".variant";
         ShipVariantAPI variant = loadVariant(modId, path);
         if(variant == null) { // search other mods
             for(ModSpecAPI mod : Global.getSettings().getModManager().getEnabledModsCopy()) {
@@ -55,6 +56,7 @@ public class ModdedVariantsData {
             VARIANTS.put(variantId, variant);
             return false;
         } else {
+            log.info(CommonStrings.MOD_ID + ": could not find or successfully load " + path + " in any enabled mod");
             return true;
         }
     }
@@ -72,29 +74,40 @@ public class ModdedVariantsData {
     //Credit to Rubi
     public static ShipVariantAPI loadVariant(String modId, String path) {
         ShipVariantAPI variant = null;
+
+        JSONObject obj = null;
         try {
-            JSONObject obj = Global.getSettings().loadJSON(path, modId);
-            String displayName = obj.getString("displayName");
-            int fluxCapacitors = obj.getInt("fluxCapacitors");
-            int fluxVents = obj.getInt("fluxVents");
+            obj = Global.getSettings().loadJSON(path, modId);
+        } catch (IOException | RuntimeException e) {
+            // RuntimeException gets thrown in practice as of 0.97a
+            return null;
+        } catch (JSONException e) {
+            log.info(CommonStrings.MOD_ID + ": could not parse JSON from " + modId + " at " + path);
+            return null;
+        }
+
+        try {
+            final String displayName = obj.getString("displayName");
+            final int fluxCapacitors = obj.getInt("fluxCapacitors");
+            final int fluxVents = obj.getInt("fluxVents");
             boolean goalVariant = false;
             try {
                 goalVariant = obj.getBoolean("goalVariant");
             } catch (JSONException ignored) {}
             String hullId = obj.getString("hullId");
-            JSONArray hullMods = obj.getJSONArray("hullMods");
+            final JSONArray hullMods = obj.getJSONArray("hullMods");
             JSONArray modules = null;
             try {
                 modules = obj.getJSONArray("modules");
             } catch (JSONException ignored) {}
-            JSONArray permaMods = obj.getJSONArray("permaMods");
+            final JSONArray permaMods = obj.getJSONArray("permaMods");
             JSONArray sMods = null;
             try {
                 sMods = obj.getJSONArray("sMods");
             } catch (JSONException ignored) {}
             //float quality = (float) obj.getDouble("quality"); not used/available in API
             String variantId = obj.getString("variantId");
-            JSONArray weaponGroups = obj.getJSONArray("weaponGroups");
+            final JSONArray weaponGroups = obj.getJSONArray("weaponGroups");
             JSONArray wings = null;
             try {
                 wings = obj.getJSONArray("wings");
@@ -105,18 +118,28 @@ public class ModdedVariantsData {
             variant.setNumFluxCapacitors(fluxCapacitors);
             variant.setNumFluxVents(fluxVents);
             variant.setGoalVariant(goalVariant);
+
+
+
             // todo: check if order matters
             if (sMods != null) {
                 for (int k = 0; k < sMods.length(); k++) {
-                    String sModId = hullMods.getString(k);
+                    final String sModId = sMods.getString(k);
+                    if(Global.getSettings().getHullModSpec(sModId) == null) {
+                        log.info(CommonStrings.MOD_ID + ": unknown sMod \"" + sModId + "\" from " + modId + " at " + path);
+                        return null;
+                    }
                     variant.addPermaMod(sModId, true);
-//                    variant.addPermaMod(sModId);
                     variant.addMod(sModId);
                 }
             }
             if(permaMods != null){
                 for (int j = 0; j < permaMods.length(); j++) {
-                    String permaModId = hullMods.getString(j);
+                    final String permaModId = permaMods.getString(j);
+                    if(Global.getSettings().getHullModSpec(permaModId) == null) {
+                        log.info(CommonStrings.MOD_ID + ": unknown permaMod \"" + permaModId + "\" from " + modId + " at " + path);
+                        return null;
+                    }
                     variant.addPermaMod(permaModId);
                     if(!variant.getHullMods().contains(permaModId)){
                         variant.addMod(permaModId);
@@ -125,7 +148,11 @@ public class ModdedVariantsData {
             }
             if(hullMods != null){
                 for (int i = 0; i < hullMods.length(); i++) {
-                    String hullModId = hullMods.getString(i);
+                    final String hullModId = hullMods.getString(i);
+                    if(Global.getSettings().getHullModSpec(hullModId) == null) {
+                        log.info(CommonStrings.MOD_ID + ": unknown hullMod \"" + hullModId + "\" from " + modId + " at " + path);
+                        return null;
+                    }
                     if(!variant.getHullMods().contains(hullModId)){
                         variant.addMod(hullModId);
                     }
@@ -133,13 +160,17 @@ public class ModdedVariantsData {
             }
             if (modules != null) {
                 for (int m = 0; m < modules.length(); m++) {
-                    JSONObject module = modules.getJSONObject(m);
+                    final JSONObject module = modules.getJSONObject(m);
                     // todo this is a very inefficient way to do it (obj length always == 1)
                     //  but I don't want to deal with Iterators
-                    JSONArray slots = module.names();
+                    final JSONArray slots = module.names();
                     for (int s = 0; s < slots.length(); s++) {
-                        String slotId = slots.getString(s);
-                        String moduleVariantId = module.getString(slotId);
+                        final String slotId = slots.getString(s);
+                        final String moduleVariantId = module.getString(slotId);
+                        if(Global.getSettings().getVariant(moduleVariantId) == null) {
+                            log.info(CommonStrings.MOD_ID + ": unknown module \"" + moduleVariantId + "\" from " + modId + " at " + path);
+                            return null;
+                        }
                         //todo *** Given moduleVariantId instead of path, create ShipVariantAPI using loadVariant() ***
                         variant.setModuleVariant(slotId, Global.getSettings().getVariant(moduleVariantId));
                     }
@@ -147,15 +178,25 @@ public class ModdedVariantsData {
             }
             // todo maybe you can do something better with variant.getNonBuiltInWeaponSlots()?
             for (int wg = 0; wg < weaponGroups.length(); wg++) {
-                WeaponGroupSpec weaponGroupSpec = new WeaponGroupSpec(WeaponGroupType.LINKED);
-                JSONObject weaponGroup = weaponGroups.getJSONObject(wg);
-                boolean autofire = weaponGroup.getBoolean("autofire");
-                String mode = weaponGroup.getString("mode");
-                JSONObject weapons = weaponGroup.getJSONObject("weapons");
-                JSONArray slots = weapons.names();
+                final WeaponGroupSpec weaponGroupSpec = new WeaponGroupSpec(WeaponGroupType.LINKED);
+                final JSONObject weaponGroup = weaponGroups.getJSONObject(wg);
+                final boolean autofire = weaponGroup.getBoolean("autofire");
+                final String mode = weaponGroup.getString("mode");
+                final JSONObject weapons = weaponGroup.getJSONObject("weapons");
+                final JSONArray slots = weapons.names();
                 for (int s = 0; s < slots.length(); s++) {
-                    String slotId = slots.getString(s);
-                    String weaponId = weapons.getString(slotId);
+                    final String slotId = slots.getString(s);
+                    final String weaponId = weapons.getString(slotId);
+                    // getWeaponSpec seems to throw an exception when invalid id is given rather than returning null
+                    try {
+                        if(Global.getSettings().getWeaponSpec(weaponId) == null) {
+                            log.info(CommonStrings.MOD_ID + ": unknown weapon \"" + weaponId + "\" from " + modId + " at " + path);
+                            return null;
+                        }
+                    } catch (Exception e) {
+                        log.info(CommonStrings.MOD_ID + ": unknown weapon \"" + weaponId + "\" from " + modId + " at " + path);
+                        return null;
+                    }
                     variant.addWeapon(slotId, weaponId);
                     weaponGroupSpec.addSlot(slotId);
                 }
@@ -164,14 +205,29 @@ public class ModdedVariantsData {
                 variant.addWeaponGroup(weaponGroupSpec);
             }
             if (wings != null) {
-                int numBuiltIn = Global.getSettings().getVariant(variant.getHullSpec().getHullId() + "_Hull").getFittedWings().size();
+                final int numBuiltIn = Global.getSettings().getVariant(variant.getHullSpec().getHullId() + "_Hull").getFittedWings().size();
                 for (int w = 0; w < wings.length(); w++) {
-                    variant.setWingId(numBuiltIn + w, wings.getString(w));
+                    final String wingId = wings.getString(w);
+                    // getFighterWingSpec seems to throw an exception when invalid id is given rather than returning null
+                    try {
+                        if(Global.getSettings().getFighterWingSpec(wingId) == null) {
+                            log.info(CommonStrings.MOD_ID + ": unknown weapon \"" + wingId + "\" from " + modId + " at " + path);
+                            return null;
+                        }
+                    } catch (Exception e) {
+                        log.info(CommonStrings.MOD_ID + ": uunknown weapon \"" + wingId + "\" from " + modId + " at " + path);
+                        return null;
+                    }
+                    variant.setWingId(numBuiltIn + w, wingId);
                 }
             }
         } catch (Exception e) {
-            log.info("could not load ship variant from " + modId + " at " + path);
-            variant = null;
+            log.info(e);
+            for(final StackTraceElement st : e.getStackTrace()) {
+                log.info(st);
+            }
+            log.info(CommonStrings.MOD_ID + ": error loading variant from " + modId + " at " + path);
+            return null;
         }
         
         return variant;
